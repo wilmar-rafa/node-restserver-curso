0f84a850462bc42002d98fba4ccea70cac7f7952
Wilmar <wilmarrafa@hotmail.com>
2020-04-21 21:39:26 -0500

    Variable de entorno para conexion BD Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
     urlDB='mongodb://localhost:27017/cafe';
 }else{
-    urlDB='mongodb+srv://wilmarrafa:holaMundo1@cluster0-fzxp9.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB=process.env.MONGO_URI;
 }
 
 process.env.URLDB=urlDB
